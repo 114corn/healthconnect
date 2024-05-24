63d74f99555d0bd5a6c17392c1ff28efe7f5734b
114corn <163351509+114corn@users.noreply.github.com>
2024-05-24 04:40:26 +0200

    improve code readability

--- usePatientData.ts
+++ usePatientData.ts
@@ -13,18 +13,23 @@ export const usePatientData = () => {
   const [error, setError] = useState<string | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
+  const baseURL = process.env.REACT_APP_BACKEND_URL;
+
+  const handleError = (err: any) => {
+    if (axios.isAxiosError(err)) {
+      setError(err.response?.data.message || 'An error occurred while processing your request.');
+    } else {
+      setError('An unexpected error occurred.');
+    }
+  };
+
   const fetchData = async () => {
     setLoading(true);
     try {
-      const response = await axios.get<PatientData[]>(`${process.env.REACT_APP_BACKEND_URL}/patients`);
+      const response = await axios.get<PatientData[]>(`${baseURL}/patients`);
       setPatientData(response.data);
     } catch (err) {
-      if (axios.isAxiosError(err)) {
-        // This checks if the error is from Axios, providing more information
-        setError(err.response?.data || 'Failed to fetch patient data.');
-      } else {
-        setError('An unexpected error occurred.');
-      }
+      handleError(err);
     } finally {
       setLoading(false);
     }
@@ -33,15 +38,10 @@ export const usePatientData = () => {
   const updateData = async (patientId: string, newData: Partial<PatientData>) => {
     setLoading(true);
     try {
-      await axios.put(`${process.env.REACT_APP_BACKEND_URL}/patients/${patientId}`, newData);
-      await fetchData();
+      await axios.put(`${baseURL}/patients/${patientId}`, newData);
+      await fetchData(); // Refresh data after update
     } catch (err) {
-      if (axios.isAxiosError(err)) {
-        // More detailed error information from Axios
-        setError(err.response?.data || 'Failed to update patient data.');
-      } else {
-        setError('An unexpected error occurred during update.');
-      }
+      handleError(err);
     } finally {
       setLoading(false);
     }
@@ -49,10 +49,7 @@ export const usePatientData = () => {
 
   useEffect(() => {
     fetchData();
-    // Although the original code didn't include `fetchData` in the dependency array,
-    // it's a common recommendation to include functions used inside useEffect
-    // unless you are absolutely sure they will not change or cause re-renders.
-  }, [fetchData]);
+  }, []);
 
   return { patientData, error, loading, updatePatientData: updateData };
 };
